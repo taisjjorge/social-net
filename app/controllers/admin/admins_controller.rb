165ca0eec3bd28631ffb6256bd8a787c14a2520e
tais.amanda <tais.amanda@g.globo>
2023-04-20 20:47:36 -0300

    feat: Finalize crud for app

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -8,51 +8,45 @@ class Admin::AdminsController < AdminController
     def new
       @admin = Admin.new
     end
+  
+    def create
+      @admin = Admin.new(form_params)
+  
+      if @admin.save
+        redirect_to admin_admins_path
+      else
+        render :new, status: 422
+      end
+    end
 
+    def edit
+    end
+
+    def update
+      params = form_params.to_h
+      params = params.except!(:password, :password_confirmation) if params[:password].blank?
+  
+      if @admin.update(params)
+        redirect_to admin_admins_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @admin = Admin.find(params[:id])
+      @admin.destroy
+      redirect_to admin_admins_path
+    end
+  
     private
     
     def form_params
-      params.require(:admin).permit(:name, :email, :password_confirmation)
+      params.require(:admin).permit(:name, :email, :password, :password_confirmation)
     end
   
-    # def create
-    #   @admin = Admin.new(form_params)
-  
-    #   if @admin.save
-    #     redirect_to admin_admins_path
-    #   else
-    #     render :new
-    #   end
-    # end
-  
-    # def edit
-    # end
-  
-    # def update
-    #   params = form_params.to_h
-    #   params = params.except!(:password, :password_confirmation) if params[:password].blank?
-  
-    #   if @admin.update(params)
-    #     redirect_to admin_admins_path
-    #   else
-    #     render :edit
-    #   end
-    # end
-  
-    # def destroy
-    #   @admin.destroy
-    #   redirect_to admin_admins_path
-    # end
-  
-    # private
-  
-    # def form_params
-    #   params.require(:admin).permit(:name, :email, :password, :password_confirmation)
-    # end
-  
-    # def set_admin
-    #   @admin = Admin.find(params[:id])
-    # end
-  
+    def set_admin
+      @admin = Admin.find(params[:id])
+    end
   end
   
